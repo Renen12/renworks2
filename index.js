3cae10e27367486b246da05153a88d91f4deb1b6
Olle Modin <ollemodin@outlook.com>
2025-03-29 10:15:55 +0100

    Some basic utility functions

---
+++ index.js
@@ -0,0 +1,50 @@
+export let renWorks = {
+    /**
+ * 
+ * @param {string} selector 
+ * @returns {HTMLElement}
+ */
+    get(selector) {
+        let element = document.querySelector(selector);
+        if (element == null || element == undefined) {
+            throw new Error(`Cannot find an element with the identifier ${selector}. Perhaps it has not loaded in or does not exist.`);
+        }
+        return element;
+    },
+    /**
+     * 
+     * @param {HTMLElement|Document} parent 
+     * @param {string} selector 
+     * @returns {HTMLElement}
+     */
+    qa(parent, selector) {
+        return parent.querySelectorAll(selector);
+    },
+    /**
+     * 
+     * @param {any[]} collection 
+     * @param {Function|(value, index)} fn 
+     */
+    enumerate(collection, fn) {
+        for (let i = 0; i < collection.length; i++) {
+            fn(collection[i], i);
+        }
+    },
+    /**
+     * 
+     * @param {{}[]} array 
+     * @param {string[]} propertyAndDesiredValue 
+     */
+    // ["name", "test"] -> Look for property "name" with value "test"
+    matchingObj(array, propertyAndDesiredValue) {
+        let desired = null;
+        this.enumerate(array, (obj) => {
+            if (obj[propertyAndDesiredValue[0]] == propertyAndDesiredValue[1]) {
+                desired = obj;
+            }
+        });
+        if (desired == null) {throw new Error("No such object found.")};
+        return desired;
+    }
+    
+}
